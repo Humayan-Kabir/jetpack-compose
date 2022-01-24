198cf92ff553c0ab4c33bdfcd1f97005a28d0bfb
Humayan-Kabir <zyxwv.hk@gmail.com>
2022-01-24 19:41:10 +0600

    default setup for starting jetpack compose

--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -3,15 +3,8 @@ package com.example.learnjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
 
@@ -20,32 +13,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(verticalArrangement = Arrangement.Top) {
-                        Greeting(name = "Hello Noob")
-                        Greeting(name = "Jetpack Compose hello I am gaining in ...")
-                    }
-                }
+                Greeting(greeting = "Jetpack Compose")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(greeting: String) {
+    Text(text = greeting)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start) {
-                Greeting(name = "Noob")
-                Greeting(name = "Jetpack Compose I am gaining in ...")
-            }
+        LearnJetpackComposeTheme {
+            Greeting(greeting = "Jetpack Compose")
         }
     }
 }
